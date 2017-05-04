c5ba3e0785218162f33c3aa6ec38918ca1872211
AliaksandrStaliarou <staliarou@gmail.com>
2017-05-04 11:03:57 +0300

    ...

--- todo_app.js
+++ todo_app.js
@@ -6,21 +6,10 @@
     });
 
 
-    function renderList() {
-        // renderTodos(todosFromLocalStorage);
-        var lsTodos = TodoManager.loadTodos();
-
-        var htmlTodos;
-        for (var i = 0; i < lsTodos.length; i++) {
-            htmlTodos += '<li><label><input type="checkbox">' + lsTodos[i] + '</label></li>';
-        }
-
-        document.getElementById('todos').innerHTML = htmlTodos;
-    }
-
-
     function initApp() {
-        TodoManager.init();
+        TodoManager.init(); //инициализирует туду манагер
+        var list = TodoManager.loadTodos(); //получает список тудушек из манагера
+        renderList(list); //рендерит список, добавляет его на страницу
 
         //bind handlers to checkboxes
         var checkboxesList = document.body.querySelectorAll('label > input');
@@ -38,22 +27,39 @@
         }
     }
 
+    function renderList() {
+        // renderTodos(todosFromLocalStorage);
+        //получил список тудушек
+        var lsTodos = TodoManager.loadTodos();
+
+        //отрендерил
+        var htmlTodos;
+        for (var i = 0; i < lsTodos.length; i++) {
+            htmlTodos += '<li><label><input type="checkbox">' + lsTodos[i] + '</label></li>';
+        }
+
+        //вставил
+        document.getElementById('todos').innerHTML = htmlTodos;
+    }
+
 
     function addTodo() {
         // save todo to LS
         var todoText = document.getElementById('task').value;
 
-        TodoManager.addTodo({
-            text: todoText
-        });
-
         // add todo to list and bind checkbox event
-        if (todoText) {
-            var todoString = '<label>' + '<input type="checkbox">' + todoText + '</label>';
+        if (todoText) { //проверяет, не пустой ли текст у тудушки
+
+            TodoManager.addTodo({ //если не пустой, то добавляет тудушку в todoManager
+                text: todoText
+            });
+
+            var todoString = '<label>' + '<input type="checkbox">' + todoText + '</label>'; //рендерит тудушку
+
             var li = document.createElement('li');
             li.innerHTML = todoString;
 
-            document.body.querySelector('ul').appendChild(li);
+            document.body.querySelector('ul').appendChild(li); //добавляет её на страницу
         }
     }
 
@@ -83,7 +89,6 @@
         // uses renderTodo for each todo in list and returns contcatenated string
         return '<li><label><input type="checkbox">' + todo.text + '</label></li>'
     }
-
 
     renderList();
 /*    renderTodo({
